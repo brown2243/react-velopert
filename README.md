0effb7cee557a2d7bd3439b1160bcc79969bebe1
brown <brown2243@naver.com>
2021-06-23 22:20:32 +0900

    chap.2 done

--- README.md
+++ README.md
@@ -54,10 +54,96 @@ justify-content: center;
 align-items: center;
 ```
 
-아니 열심히 커밋한거 git config 회사계정이라 커밋로그에 기록이 안됐다.
-근데 그러면 회사계정꺼에는 기록해줘야지!!
+조건부로 CSS 클래스를 넣어주고 싶을때 이렇게 문자열을 직접 조합해주는 것 보다 classnames 라는 라이브러리를 사용하는 것이 훨씬 편합니다.
+classNames 를 사용하면 다음과 같이 조건부 스타일링을 할 때 함수의 인자에 문자열, 배열, 객체 등을 전달하여 손쉽게 문자열을 조합 할 수 있습니다.
+
+```
+classNames('foo', 'bar'); // => 'foo bar'
+classNames('foo', { bar: true }); // => 'foo bar'
+classNames({ 'foo-bar': true }); // => 'foo-bar'
+classNames({ 'foo-bar': false }); // => ''
+classNames({ foo: true }, { bar: true }); // => 'foo bar'
+classNames({ foo: true, bar: true }); // => 'foo bar'
+classNames(['foo', 'bar']); // => 'foo bar'
+
+// 동시에 여러개의 타입으로 받아올 수 도 있습니다.
+classNames('foo', { bar: true, duck: false }, 'baz', { quux: true }); // => 'foo bar baz quux'
+
+// false, null, 0, undefined 는 무시됩니다.
+classNames(null, false, 'bar', undefined, 0, 1, { baz: null }, ''); // => 'bar 1'
+```
+
+반복이 되는 코드는 Sass 의 mixin 이라는 기능을 사용하여 쉽게 재사용 할 수 있습니다. 한번, button-color라는 mixin 을 만들어서 사용해보겠습니다.
+
+```
+  &.blue {
+    background: $blue;
+    &:hover {
+      background: lighten($blue, 10%);
+    }
+
+    &:active {
+      background: darken($blue, 10%);
+    }
+  }
+
+  &.gray {
+    background: $gray;
+    &:hover {
+      background: lighten($gray, 10%);
+    }
+
+    &:active {
+      background: darken($gray, 10%);
+    }
+  }
+
+  &.pink {
+    background: $pink;
+    &:hover {
+      background: lighten($pink, 10%);
+    }
+
+    &:active {
+      background: darken($pink, 10%);
+    }
+  }
+
+@mixin button-color($color) {
+  background: $color;
+  &:hover {
+    background: lighten($color, 10%);
+  }
+  &:active {
+    background: darken($color, 10%);
+  }
+}
+  // 색상 관리
+  &.blue {
+    @include button-color($blue);
+  }
+
+  &.gray {
+    @include button-color($gray);
+  }
+
+  &.pink {
+    @include button-color($pink);
+  }
+
+```
+
+...rest props 전달하기
+
+spread 와 rest 입니다. 이 문법은 주로 배열과 객체, 함수의 파라미터, 인자를 다룰 때 사용하는데, 컴포넌트에서도 사용 할 수 있답니다.
+
+이렇게 컴포넌트가 어떤 props 를 받을 지 확실치는 않지만 그대로 다른 컴포넌트 또는 HTML 태그에 전달을 해주어야 하는 상황에는 이렇게 ...rest 문법을 활용하시면 됩니다!
 
 ## CSS Module
+
+CSS Module 이라는 기술을 사용하면, CSS 클래스가 중첩되는 것을 완벽히 방지할 수 있습니다.
+
+CRA 로 만든 프로젝트에서 CSS Module 를 사용 할 때에는, CSS 파일의 확장자를 .module.css 로 하면 되는데요, 예를 들어서 다음과 같이 Box.module.css 라는 파일을 만들게 된다면
 
 ## styled-component
 
--- src/App.js
+++ src/App.js
@@ -1,14 +1,182 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Button from './components/Button';
+import CheckBox from './components/CheckBox';
+import styled, { css, ThemeProvider } from 'styled-components';
+import Button2 from './components/Button2';
+import Dialog from './components/Dialog';
+
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background: ${props => props.color || 'black'};
+  border-radius: 50%;
+  ${props => props.huge
+    && css`
+      width: 10rem;
+      height: 10rem;
+    `}
+`;
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
+`;
+const ButtonGroup = styled.div`
+  & + & {
+    margin-top: 1rem;
+  }
+`;
 
 function App() {
+  const [check, setCheck] = useState(false);
+  const onChange = e => {
+    setCheck(e.target.checked);
+  };
+
+  const [dialog, setDialog] = useState(false);
+  const onClick = () => {
+    setDialog(true);
+  };
+  const onConfirm = () => {
+    console.log('확인');
+    setDialog(false);
+  };
+  const onCancel = () => {
+    console.log('취소');
+    setDialog(false);
+  };
+
   return (
     <div className="App">
-      <div className="buttons">
-        <Button>BUTTON</Button>
+      <ThemeProvider
+        theme={{
+          palette: {
+            blue: '#228be6',
+            gray: '#495057',
+            pink: '#f06595'
+          }
+        }}
+      >
+        <AppBlock>
+          <ButtonGroup>
+            <Button size="large">BUTTON</Button>
+            <Button>BUTTON</Button>
+            <Button size="small">BUTTON</Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button color="gray" size="large">
+              BUTTON
+            </Button>
+            <Button color="gray">BUTTON</Button>
+            <Button color="gray" size="small">
+              BUTTON
+            </Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button color="pink" size="large">
+              BUTTON
+            </Button>
+            <Button color="pink">BUTTON</Button>
+            <Button color="pink" size="small">
+              BUTTON
+            </Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button size="large" outline>
+              BUTTON
+            </Button>
+            <Button color="gray" outline>
+              BUTTON
+            </Button>
+            <Button color="pink" size="small" outline>
+              BUTTON
+            </Button>
+          </ButtonGroup>
+          <ButtonGroup>
+            <Button size="large" fullWidth>
+              BUTTON
+            </Button>
+            <Button size="large" color="gray" fullWidth>
+              BUTTON
+            </Button>
+            <Button size="large" color="pink" fullWidth>
+              BUTTON
+            </Button>
+          </ButtonGroup>
+        </AppBlock>
+        <Dialog
+          title="정말로 삭제하시겠습니까?"
+          confirmText="삭제"
+          cancelText="취소"
+          onConfirm={onConfirm}
+          onCancel={onCancel}
+          visible={dialog}
+        >
+          데이터를 정말로 삭제하시겠습니까?
+        </Dialog>
+
+      </ThemeProvider>
+      <Circle color="red" huge />
+      <div>
+        <CheckBox onChange={onChange} checked={check}>
+          다음 약관에 모두 동의
+        </CheckBox>
+        <p>
+          <b>check: </b>
+          {check ? 'true' : 'false'}
+        </p>
       </div>
-    </div>
+
+
+      <div className="buttons">
+        <Button size="large" onClick={() => console.log('클릭됐다!')}>BUTTON</Button>
+        <Button>BUTTON</Button>
+        <Button size="small">BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="gray">
+          BUTTON
+        </Button>
+        <Button color="gray">BUTTON</Button>
+        <Button size="small" color="gray">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="pink">
+          BUTTON
+        </Button>
+        <Button color="pink">BUTTON</Button>
+        <Button size="small" color="pink">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="blue" outline>
+          BUTTON
+        </Button>
+        <Button color="gray" outline>
+          BUTTON
+        </Button>
+        <Button size="small" color="pink" outline>
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" fullWidth>
+          BUTTON
+        </Button>
+        <Button size="large" fullWidth color="gray">
+          BUTTON
+        </Button>
+        <Button size="large" fullWidth color="pink">
+          BUTTON
+        </Button>
+      </div>
+    </div >
   );
 }
 
